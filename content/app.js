67c96f2ddcad451a42a35d1818d4c015943ed08a
Andrew Chen <ac12491@gmail.com>
2017-03-21 23:59:03 -0600

    angular  project.

---
+++ content/app.js
@@ -0,0 +1,34 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.controller('ninjaController', ['$scope', function($scope){
+  $scope.ninjas = [
+    {
+      name: 'Andrew',
+      belt: 'orange',
+      rate: 145.67,
+      thumb: 'http://i.telegraph.co.uk/multimedia/archive/03519/potd-squirrel_3519920k.jpg',
+      available: true
+    },
+    {
+      name: 'David',
+      belt: 'blue',
+      rate: 123.12,
+      thumb: 'http://i.telegraph.co.uk/multimedia/archive/03519/potd-bat_3519917k.jpg',
+      available: true
+    },
+    {
+      name: 'Paul',
+      belt: 'red',
+      rate: 454.32,
+      thumb: 'http://i.telegraph.co.uk/multimedia/archive/03519/potd-sunfish_3519926k.jpg',
+      available: false
+    },
+    {
+      name: 'Eric',
+      belt: 'white',
+      rate: 423.76,
+      thumb: 'http://i.telegraph.co.uk/multimedia/archive/03519/potd-malham-cove-2_3519750k.jpg',
+      available: true
+    }
+  ]
+}]);
